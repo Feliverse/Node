2902647a0700192963380c2bc245ae9da3687a9d
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-11-07 18:02:54 -0400

    gitignore

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const { frutas, dinero } = require('./frutas');
+import chalk from 'chalk';
 
 frutas.forEach((fruta, index) => {
   console.log(index, fruta);
 });
 console.log((dinero.toLocaleString('en-US', { style: 'currency', currency: 'USD' })));
 
-
+console.log(chalk.blue('Hello world!'));
 
   // versionado de proyectos:  MAJOR.MINOR.PATCH   SEMANTIC VERSIONING
 
