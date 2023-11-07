e6d3ac0f543565e0a71e2fa6535b00dcb56f4b0c
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-11-07 17:49:24 -0400

    install chalk

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const {frutas, dinero} = require('./frutas');
+const { frutas, dinero } = require('./frutas');
+const chalk = require('chalk');
+
+
 
 frutas.forEach((fruta, index) => {
   console.log(index, fruta);
-})
-  console.log(dinero);
+});
+console.log(chalk.red(dinero));
+
 
 
   // versionado de proyectos:  MAJOR.MINOR.PATCH   SEMANTIC VERSIONING
 
   // npm init -y   es como decir yes a todo
-  
